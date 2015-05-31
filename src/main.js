7ac40616de0cbf043bdba0f518c2a05e881b7677
Colby Swandale <colby@taplaboratories.com.au>
2015-05-31 22:45:54 +1000

    hide/show on main window close

--- src/main.js
+++ src/main.js
@@ -1,38 +1,47 @@
 'use strict';
 
 var app = require('app');
+var clipboard = require('clipboard');
 var BrowserWindow = require('browser-window');
 var globalShortcut = require('global-shortcut');
 
 // start the crash reporter
 require('crash-reporter').start();
-
 // create a global variable for the main Window
 var mainWindow = null;
-
-// quit when all windows are closed except on OS X
-app.on('window-all-closed', function() {
-  if(process.platform != 'darwin') { app.quit(); }
-});
+// a flag that indicates if the user is quitting the app
+// we use this on the browser window close event to
+var willQuit = false;
 
 app.on('ready', function() {
+  // load jquery
   mainWindow = new BrowserWindow({width: 1110, height:640});
   mainWindow.loadUrl('https://play.google.com/music');
+
+  mainWindow.on('close', function(e) {
+    if (willQuit) { return true; }
+    e.preventDefault();
+    mainWindow.hide();
+  });
 
   // register shortcusts for the media keys
   globalShortcut.register('medianexttrack', function() {
     mainWindow.webContents.executeJavaScript('document.querySelector(\'sj-icon-button[data-id="forward"]\').click();');
   });
-  
+
   globalShortcut.register('mediaplaypause', function() {
     mainWindow.webContents.executeJavaScript('document.querySelector(\'sj-icon-button[data-id="play-pause"]\').click();');
-  });
+  })
 
   globalShortcut.register('mediaprevioustrack', function() {
     mainWindow.webContents.executeJavaScript('document.querySelector(\'sj-icon-button[data-id="rewind"]\').click();');
   });
+});
 
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  })
+app.on('before-quit', function() {
+  willQuit = true;
+});
+
+app.on('activate-with-no-open-windows', function() {
+  mainWindow.show();
 });
